682014df8e2ab738853f86fcb7a5bccf5a3c26ff
feebeef <phoebe_chen@dlsu.edu.ph>
2020-07-12 17:07:48 +0800

    code

--- 1-homework/hw1.py
+++ 1-homework/hw1.py
@@ -1,25 +1,9 @@
+file  = open("starwars.txt", "r") 
+x = file.read()
 
-
-
-a = w[1000:2000]
-
-# print(len(a))
-# print(a[len(a)-1])
-res = {}
-
-def recursion(l):
-    if(len(l) == 0):
-        print("none")
-    else:
-        temp = re.findall(l[0], ''.join(l))
-        res[temp[0]] = len(temp)
-        filtered = filter(lambda i: i != temp[0] , l) 
-        recursion(filtered)
-
-recursion(a)
-
-items = res.items()
-
-sorted_items = sorted(res.items(), key=operator.itemgetter(1))
-print(sorted_items)
+#https://www.debuggex.com/cheatsheet/regex/python
+#https://pythex.org/
+import re
+words = re.findall("(\w+-*\w*'*\w*)",x)
+print('total: ' + str(len(words)))
 
