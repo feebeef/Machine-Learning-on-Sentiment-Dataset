dea55eb0bdac296891a263e84dbce77b348db944
Phoebe Chen <47266501+feebeef@users.noreply.github.com>
2022-07-03 23:31:34 +0800

    Delete 1-homework directory

--- 1-homework/hw1.py
+++
@@ -1,9 +0,0 @@
-file  = open("starwars.txt", "r") 
-x = file.read()
-
-#https://www.debuggex.com/cheatsheet/regex/python
-#https://pythex.org/
-import re
-words = re.findall("(\w+-*\w*'*\w*)",x)
-print('total: ' + str(len(words)))
-
